56d2221783cacec5436cd76da305a788e9683fbd
Ayo Dahunsi <ayo@dolasoft.com>
2024-05-22 00:26:45 -0600

    Updated cshtml

--- src/AspNetRazor/Properties/AssemblyInfo.cs
+++ src/AspNetRazor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]  
 [assembly: AssemblyCompany("ZeraSystems Inc.")]  
 [assembly: AssemblyProduct("CodeStencil Code Nanites")]  
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]  
 [assembly: AssemblyCulture("")]  
   
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers   
 // by using the '*' as shown below:  
 // [assembly: AssemblyVersion("1.0.*")]  
-[assembly: AssemblyVersion("2.2.3")]  
-[assembly: AssemblyFileVersion("2.2.3")]  
+[assembly: AssemblyVersion("2.2.6")]  
+[assembly: AssemblyFileVersion("2.2.6")]  
