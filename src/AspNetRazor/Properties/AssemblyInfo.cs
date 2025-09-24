2a834194be0a1ffedeb9302e3983200c970a1e86
Ayo Dahunsi <ayo@zerasystems.com>
2025-09-24 02:48:28 -0600

    Update version, namespaces, and binary file changes
    Updated the assembly version and file version in `AssemblyInfo.cs`
    from 2.2.7 to 2.2.8.0, indicating a minor update. Added new `using`
    directives in `AspNetRazor.cs` to include `System.Collections.Generic`,
    `System.Text`, and `System.Threading.Tasks`. Modified the binary file
    `aspnetcore-razor.codestencil` (details not available). Addressed a
    potential formatting or encoding issue in the copyright year.

--- src/AspNetRazor/Properties/AssemblyInfo.cs
+++ src/AspNetRazor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]  
 [assembly: AssemblyCompany("ZeraSystems Inc.")]  
 [assembly: AssemblyProduct("CodeStencil Code Nanites")]  
-[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyCopyright("Copyright ï¿½ 2025")]
 [assembly: AssemblyTrademark("")]  
 [assembly: AssemblyCulture("")]  
   
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers   
 // by using the '*' as shown below:  
 // [assembly: AssemblyVersion("1.0.*")]  
-[assembly: AssemblyVersion("2.2.7")]  
-[assembly: AssemblyFileVersion("2.2.7")]  
+[assembly: AssemblyVersion("2.2.8.0")]  
+[assembly: AssemblyFileVersion("2.2.8.0")]  
